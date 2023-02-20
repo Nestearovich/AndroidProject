db49b9b6481637307feec5b72dc5de9ba07d6ccf
Nestearovich <dante_vit@mail.ru>
2023-02-20 20:22:38 +0300

    lesson 39 RxJava

--- app/src/main/java/com/example/androidproject/presentation/home/items/FavoritesFragment.kt
+++ app/src/main/java/com/example/androidproject/presentation/home/items/FavoritesFragment.kt
@@ -1,15 +1,20 @@
 package com.example.androidproject.presentation.home.items
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContentProviderCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidproject.App
 import com.example.androidproject.databinding.FragmentFavoritsBinding
 import com.example.androidproject.presentation.home.items.adapter.FavoritesAdapter
+import com.example.androidproject.presentation.receiver.AirplaneModeChangeReceiver
+import com.example.androidproject.presentation.receiver.MyBroadcastReceiver
 import com.example.androidproject.utils.BaseFragment
 
 
@@ -32,8 +37,20 @@ class FavoritesFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         (requireActivity().applicationContext as App).provideAppComponent().inject(this)
+
+
+        val receiver = AirplaneModeChangeReceiver()
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            requireContext().registerReceiver(receiver, it)
+        }
+
+       val receiver2 = MyBroadcastReceiver()
+        IntentFilter("MY_ACTION").also {
+            requireContext().registerReceiver(receiver2, it)
+        }
+
+        setMessage()
 
         favAdapter = FavoritesAdapter()
         viewBinding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -45,5 +62,10 @@ class FavoritesFragment : BaseFragment() {
         viewModel.favorites.observe(viewLifecycleOwner){
             favAdapter.submitList(it)
         }
+    }
+    private fun setMessage(){
+        val intent = Intent("MY_ACTION")
+        intent.putExtra("KEY", "message")
+        requireContext().sendBroadcast(intent)
     }
 }
--- app/src/main/java/com/example/androidproject/presentation/home/items/SearchFragment.kt
+++ app/src/main/java/com/example/androidproject/presentation/home/items/SearchFragment.kt
@@ -3,6 +3,10 @@ package com.example.androidproject.presentation.home.items
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -51,13 +55,14 @@ class SearchFragment : BaseFragment() {
             }
 
 
+
         })
         viewModel.items.observe(viewLifecycleOwner) {
             viewBinding.description.text = it.description
             Picasso.get().load(Uri.parse(it.image)).into(viewBinding.image)
         }
 
-
+        runHandler()
 
         viewBinding.btnStart.setOnClickListener {
             requireActivity().startService(Intent(requireContext(), MusicPlayer::class.java))
@@ -66,7 +71,63 @@ class SearchFragment : BaseFragment() {
         viewBinding.btnStop.setOnClickListener {
             requireActivity().stopService(Intent(requireContext(), MusicPlayer::class.java))
         }
+
+    }
+
+   private fun runHandler() {
+        var backgroundHandler: Handler? = null
+
+        //Creating a background thread.
+        val backgroundThread = Thread {
+            //Creating a Looper for this thread.
+            Looper.prepare()
+
+            //Looper.myLooper() gives you Looper for current Thread.
+            val myLooper = Looper.myLooper()!!
+
+            //Creating a Handler for given Looper object.
+            backgroundHandler = Handler(myLooper) { msg ->
+
+                //Processing incoming messages for this Handler.
+                //Receiving extras from Message
+                val bundle: Bundle? = msg.data
+
+                Log.d("", "Handler:: Extras: ${bundle}")
+
+                Log.d("", "Handler:: Background Thread ID ${Thread.currentThread().id}")
+
+                //myLooper.quit()
+                true
+            }
+
+            Looper.loop()
+        }
+        backgroundThread.start()
+
+
+        //Click listener on a Button
+        viewBinding.btnStart.setOnClickListener {
+            Log.d("", "Handler:: UI Thread ID ${Thread.currentThread().id}")
+
+            //Executing code on backgroundThread using Handler.
+            backgroundHandler!!.post {
+                //Here, you'll note that Thread's ID is of backgroundThread.
+                Log.d("", "Handler:: Background Thread ID ${Thread.currentThread().id}")
+            }
+
+            // Now, sending data on backgroundThread using Message object. Handler's handleMessage(msg: Message?) method will receive this Message and perform appropriate action.
+            val extras = Bundle()
+            extras.putInt("PRICE", 100)
+            extras.putString("PRODUCT_NAME", "Table Lamp")
+
+            val message = Message.obtain(backgroundHandler)
+            message.data = extras
+
+            backgroundHandler?.sendMessage(message)
+        }
     }
 }
+
+
 
 
